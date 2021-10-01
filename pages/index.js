d41c2156ad2825d87fd9468b6ce26ac7ddff9874
KannaYukari <43327240+KannaYukari@users.noreply.github.com>
2021-10-01 19:38:46 -0300

    Removendo textos padrão do template da Vercel

--- pages/index.js
+++ pages/index.js
@@ -13,41 +13,32 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to my personal page!</a>
+          Seja bem-vindo(a) à minha biblioteca pessoal!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          (( Alguma descrição ))
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className={styles.card}>
+            <h2>Cartão 1 &rarr;</h2>
+            <p>Descrição do Cartão 1.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="#" className={styles.card}>
+            <h2>Cartão 2 &rarr;</h2>
+            <p>Descrição do Cartão 2.</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="#" className={styles.card}>
+            <h2>Cartão 3 &rarr;</h2>
+            <p>Descrição do Cartão 3.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="#" className={styles.card}>
+            <h2>Cartão 4 &rarr;</h2>
+            <p>Descrição do Cartão 4.</p>
           </a>
         </div>
       </main>
