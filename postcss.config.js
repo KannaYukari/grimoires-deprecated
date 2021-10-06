3bbe9532d32f923eaf3d93602105542f2f7681f0
Kanna Yukari <kanna.yukari28@gmail.com>
2021-10-06 19:47:48 -0300

    Correção do bug do tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // Purge and minify CSS only production builds only
-    ...(process.env.NODE_ENV === 'production'
-      ? { ...purgecss, cssnano: {} }
-      : {})
   }
 }
